5a32a27fd9dbc09890be44c7f35335e2fb6bb491
DoKyung <MacBook@syn-172-100-001-207.res.spectrum.com>
2025-07-11 13:09:27 +0900

    Fix:회원가입 폼 수정

--- src/components/SignUp/SignUpComplete.tsx
+++ src/components/SignUp/SignUpComplete.tsx
@@ -9,9 +9,9 @@ interface SignUpCompleteProps {
 const SignUpComplete: React.FC<SignUpCompleteProps> = ({ onPrev, onComplete }) => {
   return (
     <div className="flex flex-col items-center justify-center w-full">
-      <img src={checkIcon} alt="check" className="w-150 mb-8" />
+      <img src={checkIcon} alt="check" className="w-200 mb-8" />
       <button
-        className="px-8 py-3 bg-[#1D68FF] text-white rounded-full text-lg font-semibold shadow-md hover:bg-blue-700 transition"
+        className="w-[380px] px-20 py-5 rounded-[60px] cursor-pointer bg-[#1d68ff] text-[32px] text-white font-semibold"
         onClick={onComplete}
       >
         바로 로그인하기
