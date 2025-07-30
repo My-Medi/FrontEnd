6a7132b8056c416317f78c733e9a7669309446c8
Minkyung02 <minkyung22@swu.ac.kr>
2025-07-30 15:28:28 +0900

    Feat: 알림 취소 삭제 버튼

--- src/components/Alarm/DeleteModal.tsx
+++ src/components/Alarm/DeleteModal.tsx
@@ -8,20 +8,22 @@ interface DeleteModalProps {
 const DeleteModal: React.FC<DeleteModalProps> = ({ onConfirm, onCancel }) => {
   return (
     <div className='fixed inset-0 fixed inset-0 bg-black/30 z-40 flex justify-center items-center z-50'>
-      <div className='bg-white rounded-[20px] w-[400px] p-8 flex flex-col items-center'>
-        <h3 className='text-[#121218] text-[20px] font-semibold mb-4 text-center'>
-          알림을 삭제하시겠습니까?
-        </h3>
-        <div className='flex justify-center gap-4 mt-6'>
+      <div className='bg-white rounded-[18px] w-[587px] py-[27px] px-[62.4px] flex flex-col items-center'>
+        <div className='flex py-[60px] px-[0px] justify-center items-center gap-[6px]'>
+          <h3 className='text-[#121218] text-[24px] font-[Pretendard] tracking-[-0.72px] font-medium text-center'>
+            알림을 삭제하시겠습니까?
+          </h3>
+        </div>
+        <div className='flex justify-center gap-[30px] mt-[4.2px]'>
           <button
             onClick={onCancel}
-            className='bg-gray-300 text-[#121218] font-semibold py-2 px-5 rounded-[8px]'
+            className='flex justify-center items-center gap-[6px] w-[216px] h-[46.8px] rounded-full text-[#121218] text-[20px] font-medium font-[Pretendard] border border-[#E3E6EB] tracking-[-0.6px] shadow-[0px_0px_5px_5px_rgba(29,104,255,0.05)] cursor-pointer'
           >
             취소
           </button>
           <button
             onClick={onConfirm}
-            className='bg-[#1D68FF] text-white font-semibold py-2 px-5 rounded-[8px]'
+            className='flex justify-center bg-[#75787B] items-center gap-[6px] w-[216px] h-[46.8px] rounded-full text-[#FFF] text-[20px] font-medium font-[Pretendard] border border-[#E3E6EB] tracking-[-0.6px] shadow-[0px_0px_5px_5px_rgba(29,104,255,0.05)] cursor-pointer'
           >
             삭제
           </button>
